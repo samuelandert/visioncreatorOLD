4d51a501771bfdc8956d3b86eb766ba1450e06e1
Samuel Andert <samuel@andert.me>
2024-06-14 12:33:21 +0200

    cleanup

--- apps/app/src/lib/stores.ts
+++ apps/app/src/lib/stores.ts
@@ -4,7 +4,6 @@ import { persist, createLocalStorage } from '@macfja/svelte-persistent-store';
 interface FutureMe {
     name: string;
     visionid: string,
-    signedUp: boolean
 }
 
 const defaultFutureMe: FutureMe = { name: '', visionid: '' };
