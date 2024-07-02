24517f0836a74f030ab6de0a31b24b823ff662de
Samuel Andert <samuel@andert.me>
2024-07-02 16:24:21 +0200

    try to fix deployment bug

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
 	],
 	build: {
 		rollupOptions: {
-			preserveEntrySignatures: 'strict'
+			preserveEntrySignatures: 'strict',
+			external: ['@anthropic-ai/sdk']
 		}
 	},
 	test: {
